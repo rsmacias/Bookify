962ac7db90022fec80621538349cd12ac9963510
Robert Macias <rsmacias@gmail.com>
2023-12-16 15:19:21 -0500

    refactor: create the value objects for the concepts "Money", and "Currency" with its own validations/constraints/default values and helper functions. Include these new value objects into the domain into the Apartment entity.

--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -11,10 +11,8 @@ public sealed class Apartment : Entity
     public Name Name { get; private set; }
     public Description Description { get; private set; }
     public Address Address { get; private set; }
-    public decimal PriceAmount { get; private set; }
-    public string PriceCurrency { get; private set; }
-    public decimal CleaningFeeAmount { get; private set; }
-    public string CleaningFeeCurrency { get; private set; }
+    public Money Price { get; private set; }
+    public Money CleaningFee { get; private set; }
     public DateTime? LastBookedOnUtc { get; private set; }
     public List<Amenity> Amenities { get; private set; } = new();
 }
---
+++ src/Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,34 @@
+﻿namespace Bookify.Domain.Apartments;
+
+/// <summary>
+/// Value Object: Currency:
+/// - Define the list of the allowed currencies to the domain.
+/// - Define validation/constraints attached to the currency concept.
+/// - Expose specific utility/helper akin to the currency concept.
+/// </summary>
+public record Currency
+{
+    internal static readonly Currency None = new Currency("");
+    public static readonly Currency Usd = new Currency("USD");
+    public static readonly Currency Eur = new Currency("EUR");
+
+    /// <summary>
+    /// This is private therefore the currency values could only be obtained from the static readonly properties.
+    /// So, the allowed currencies are defined here in this record class.
+    /// </summary>
+    /// <param name="code"></param>
+    private Currency(string code) => Code = code;
+
+    public string Code { get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return  All.FirstOrDefault(c => c.Code == code) ?? throw new ApplicationException("The currency code is invalid");
+    }
+
+    public static readonly IReadOnlyCollection<Currency> All = new[]
+    {
+        Usd,
+        Eur,
+    };
+}
