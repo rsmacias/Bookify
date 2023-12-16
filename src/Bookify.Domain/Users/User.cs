94e7f6b5d309ff661fd0537bce97fa453a0301e9
Robert Macias <rsmacias@gmail.com>
2023-12-16 15:39:07 -0500

    feat: include the new entity User for the new feature Users with its own value objects.

---
+++ src/Bookify.Domain/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Users;
+
+public sealed class User : Entity
+{
+    public User(Guid id) : base(id)
+    {
+        
+    }
+
+    public FirstName FirstName { get; private set; }
+    public LastName LastName { get; private set; }
+    public Email Email { get; private set; }
+}
