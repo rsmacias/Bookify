8b06168d9549a1f37c4f88d8fab2450aac26928f
Robert Macias <rsmacias@gmail.com>
2023-12-16 16:32:18 -0500

    feat: add the objects (Entity, value objects, enums) for the Booking feature.

---
+++ src/Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,45 @@
+﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public sealed class Booking : Entity
+{
+    public Booking(
+        Guid id,
+        Guid apartmentId,
+        Guid userId,
+        DateRange duration,
+        Money priceForPeriod,
+        Money cleaningFee,
+        Money amenitiesUpCharge,
+        Money totalPrice,
+        BookingStatus status,
+        DateTime createdOnUtc
+    ) : base(id)
+    {
+        ApartmentId = apartmentId;
+        UserId = userId;
+        Duration = duration;
+        PriceForPeriod = priceForPeriod;
+        CleaningFee = cleaningFee;
+        AmenitiesUpCharge = amenitiesUpCharge;
+        TotalPrice = totalPrice;
+        Status = status;
+        CreatedOnUtc = createdOnUtc;
+    }
+
+    public Guid ApartmentId { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateRange Duration { get; private set; }
+    public Money PriceForPeriod { get; private set; }
+    public Money CleaningFee { get; private set; }
+    public Money AmenitiesUpCharge { get; private set; }
+    public Money TotalPrice { get; private set; }
+    public BookingStatus Status { get; private set; }
+    public DateTime CreatedOnUtc { get; private set; }
+    public DateTime? ConfirmedOnUtc { get; private set; }
+    public DateTime? RejectedOnUtc { get; private set; }
+    public DateTime? CompletedOnUtc { get; private set; }
+    public DateTime? CancelledOnUtc { get; private set; }
+}
