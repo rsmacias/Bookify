7605bc90d1f252f4fc5d9902f45ff7fd61cd7b0d
Robert Macias <rsmacias@gmail.com>
2023-12-16 13:55:59 -0500

    feat: add initial anemic domain model for the Apartments.

---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,19 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string Country { get; private set; }
+    public string State { get; private set; }
+    public string ZipCode { get; private set; }
+    public string City { get; private set; }
+    public string Street { get; private set; }
+    public decimal PriceAmount { get; private set; }
+    public string PriceCurrency { get; private set; }
+    public decimal CleaningFeeAmount { get; private set; }
+    public string CleaningFeeCurrency { get; private set; }
+    public DateTime? LastBookedOnUtc { get; private set; }
+    public List<Amenity> Amenities { get; private set; } = new();
+}
