1b650e2d0bbe4a6ed9f33103540d7cf673741173
Robert Macias <rsmacias@gmail.com>
2023-12-16 14:16:53 -0500

    refactor: making an abstraction from the identifier of the entities.

--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -1,8 +1,13 @@
-﻿namespace Bookify.Domain.Apartments;
+﻿using Bookify.Domain.Abstractions;
 
-public sealed class Apartment
+namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment : Entity
 {
-    public Guid Id { get; private set; }
+    public Apartment(Guid id) : base(id)
+    {
+        
+    }
     public string Name { get; private set; }
     public string Description { get; private set; }
     public string Country { get; private set; }
