2781b8e12c422bf813dd50ca4c70447c2835cbc0
Robert Macias <rsmacias@gmail.com>
2023-12-16 15:45:58 -0500

    refactor: implementing "Static Factory Pattern" into the User entity for supporting side effects (domain events) on its object creation later.

--- src/Bookify.Domain/Users/User.cs
+++ src/Bookify.Domain/Users/User.cs
@@ -4,12 +4,30 @@ namespace Bookify.Domain.Users;
 
 public sealed class User : Entity
 {
-    public User(Guid id) : base(id)
+    private User(
+        Guid id,
+        FirstName firstName,
+        LastName lastName, 
+        Email email
+    ) : base(id)
     {
-        
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
     }
 
     public FirstName FirstName { get; private set; }
     public LastName LastName { get; private set; }
     public Email Email { get; private set; }
+
+    public static User Create(FirstName firstName, LastName lastName, Email email)
+    {
+        var user = new User(Guid.NewGuid(), firstName, lastName, email);
+
+        // TODO:  
+        // - Put some other implementation details that I don't want to expose outside of this entity.
+        // - Include some side effects that don't naturally belong inside of a constructor - DOMAIN EVENTS
+
+        return user;
+    }
 }
