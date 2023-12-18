b8d8cb459caccab0fe11b45b02b2f2a64313e313
Robert Macias <rsmacias@gmail.com>
2023-12-17 19:52:25 -0500

    feat: this is my first version of the Review entity (Domain Anemic version).

---
+++ src/Bookify.Domain/Reviews/Review.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Reviews;
+
+public sealed class Review : Entity
+{
+    public Review(Guid id) : base(id)
+    {
+        
+    }
+
+    public Guid ApartmentId { get; private set; }
+    public Guid BookingId { get; private set; }
+    public Guid UserId { get; private set; }
+    public int Rating { get; private set; }
+    public string Comment { get; private set; }
+    public DateTime CreateOnUtc { get; private set; }
+}
