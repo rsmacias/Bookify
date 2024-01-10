fb559e2475f30ff6594faf515b2bbc76623a58b4
Robert Macias <rsmacias@gmail.com>
2024-01-09 20:09:23 -0500

    feat: implement event handler for the Booking reserved event. Defining a domain event handler

--- src/Bookify.Domain/Bookings/Events/BookingReservedDomainEvent.cs
+++ src/Bookify.Domain/Bookings/Events/BookingReservedDomainEvent.cs
@@ -3,4 +3,3 @@
 namespace Bookify.Domain.Bookings.Events;
 
 public sealed record BookingReservedDomainEvent(Guid BookingId) : IDomainEvent;
-// TODO: Implement logic of the side effect/event for reserving a Booking
--- src/Bookify.Domain/Bookings/IBookingRepository.cs
+++ src/Bookify.Domain/Bookings/IBookingRepository.cs
@@ -7,4 +7,6 @@ public interface IBookingRepository
     Task<bool> IsOverlappingAsync(Apartment apartment, DateRange duration, CancellationToken cancellationToken = default);
 
     void Add(Booking booking);
+
+    Task<Booking> GetByIdAsync(Guid bookingId, CancellationToken cancellationToken = default);
 }
