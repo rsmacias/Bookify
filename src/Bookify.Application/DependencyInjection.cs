e045729a3d958d06948c9495f5bf14ad912581b3
Robert Macias <rsmacias@gmail.com>
2024-01-11 23:34:06 -0500

    feat: Solving cross-cutting concern - Validation. Implementing validation pipeline for the commands by using MediatR Pipeline Behaviors.

--- src/Bookify.Application/DependencyInjection.cs
+++ src/Bookify.Application/DependencyInjection.cs
@@ -16,6 +16,8 @@ public static class DependencyInjection
             configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
 
             configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+
+            configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
 
         services.AddTransient<PricingService>();
