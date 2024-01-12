052d532b6468bb415aa901afbb6c4cb080ca76fb
Robert Macias <rsmacias@gmail.com>
2024-01-11 23:41:45 -0500

    refactor: add logic to register all the future validators to be executed and used by the Validation Pipeline.

--- src/Bookify.Application/DependencyInjection.cs
+++ src/Bookify.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Bookify.Application.Abstractions.Behaviors;
 using Bookify.Domain.Bookings;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bookify.Application;
@@ -19,6 +20,8 @@ public static class DependencyInjection
 
             configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
         services.AddTransient<PricingService>();
 
