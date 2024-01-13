0708cf78d98f40ece69a1d206b9fbd445342d87a
Robert Macias <rsmacias@gmail.com>
2024-01-13 00:06:52 -0500

    feat: adding EF Core to the solution.

---
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }   
+}
