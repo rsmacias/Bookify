da5cd00ffad3e55d922fa87fe31546ec55521b34
Robert Macias <rsmacias@gmail.com>
2024-01-12 23:44:14 -0500

    feat: configuring infrastructure services with DI - Email.EmailService

--- src/Bookify.Infrastructure/DependencyInjection.cs
+++ src/Bookify.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Email;
 using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Email;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +14,8 @@ public static class DependencyInjection
         IConfiguration configuration)
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddTransient<IEmailService, EmailService>();
 
         return services;
     }
