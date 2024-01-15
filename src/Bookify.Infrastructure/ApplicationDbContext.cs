b416f75ac4c45e200926a856e845183c89bcf097
Robert Macias <rsmacias@gmail.com>
2024-01-15 17:02:48 -0500

    feat: creating the Domain entity configurations & automatically applying entity configurations.

--- src/Bookify.Infrastructure/ApplicationDbContext.cs
+++ src/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -8,5 +8,12 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
         
-    }   
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
