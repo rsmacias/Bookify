7457a5a2bca4abcedcc040341f067da617297761
Robert Macias <rsmacias@gmail.com>
2024-01-20 14:49:29 -0500

    feat: Implementing the SQL connection factory. Implementing a type handler for Dapper gets aware of the DateOnly type.

---
+++ src/Bookify.Infrastructure/Data/DateOnlyTypeHandler.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using System.Data;
+
+namespace Bookify.Infrastructure.Data;
+
+/// <summary>
+/// This is related to Dapper because we're using DateOnly types in the date range value objects, I need to tell Dapper how to be able to map this type, because it doesn't support it out of the box.
+/// </summary>
+internal sealed class DateOnlyTypeHandler : SqlMapper.TypeHandler<DateOnly>
+{
+    public override DateOnly Parse(object value) => DateOnly.FromDateTime((DateTime)value);
+
+    public override void SetValue(IDbDataParameter parameter, DateOnly value)
+    {
+        parameter.DbType = DbType.Date;
+        parameter.Value = value;
+    }
+}
